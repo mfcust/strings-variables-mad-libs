96327b0d50f4f3fe5424b1b37501e5a29b4edaa3
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-08 23:28:17 -0700

    Create variables_and_strings.py

---
+++ variables_and_strings.py
@@ -0,0 +1,81 @@
+###Create a variable for your name and then print it to the console
+
+
+
+
+
+###Print two strings that are added together
+
+
+
+
+
+###Create a string variable, add it to another string and print the result
+
+
+
+
+
+###Create two string variables, add them together and then print the result
+
+
+
+
+
+###Use the input() function to ask a question and store the user input as a variable. Then print the variable to the console.
+
+
+
+
+
+###Mad Libs activity - Create a series of variables that store user input by asking questions. Then, create a story using these variables, strings and print functions
+'''Here's an example:
+name = input("What's your name?")
+age = input("How old are you?")
+color = input("What's your favorite color?")
+
+print("Hi, " + name + "! My name is Mr. Custance!")
+print("You said you were " + age + " years old? Wow, that's cool!")
+print("Hey " + name + ", " + color + " is my favorite color too!!")
+'''
+###Create at least 3 variables, use at least 3 print functions, and try to make it as funny and fun as possible!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###Bonus Questions
+
+###Create variable x=5 and y='5' and add them together to get '5+5' and print to the console
+
+
+
+
+
+###Create variable x=5 and y='5' and add them together to get 10 and print to the console
+
+
+
+
+
+###Print "Hello World" in one print function so that "Hello" and "World" are on different lines
+
+
+
+
+
+###Print the string "Congrats you did it!!!" 1,000 times
+
+
+
+
