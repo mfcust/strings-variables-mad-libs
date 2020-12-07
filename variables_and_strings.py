ec4eb5c2c457fe13637b52000d40779b1fd87fac
mfcust <43455391+mfcust@users.noreply.github.com>
2020-12-07 12:19:56 -0800

    Update variables_and_strings.py

--- variables_and_strings.py
+++ variables_and_strings.py
@@ -1,34 +1,34 @@
-###Create a variable for your name and then print it to the console
+#1) Create a variable for your name and then print it to the console
 
 
 
 
 
-###Print two strings that are added together
+#2) Print two strings that are added together
 
 
 
 
 
-###Create a string variable, add it to another string and print the result
+#3) Create a string variable, add it to another string and print the result
 
 
 
 
 
-###Create two string variables, add them together and then print the result
+#4) Create two string variables, add them together and then print the result
 
 
 
 
 
-###Use the input() function to ask a question and store the user input as a variable. Then print the variable to the console.
+#5) Use the input() function to ask a question and store the user input as a variable. Then print the variable to the console.
 
 
 
 
 
-###Mad Libs activity - Create a series of variables that store user input by asking questions. Then, create a story using these variables, strings and print functions
+#5) Mad Libs activity - Create a series of variables that store user input by asking questions. Then, create a story using these variables, strings and print functions
 '''Here's an example:
 name = input("What's your name?")
 age = input("How old are you?")
@@ -38,7 +38,7 @@ print("Hi, " + name + "! My name is Mr. Custance!")
 print("You said you were " + age + " years old? Wow, that's cool!")
 print("Hey " + name + ", " + color + " is my favorite color too!!")
 '''
-###Create at least 3 variables, use at least 3 print functions, and try to make it as funny and fun as possible!!
+#Create at least 3 variables, use at least 3 print functions, and try to make it as funny and fun as possible!!
 
 
 
@@ -56,25 +56,25 @@ print("Hey " + name + ", " + color + " is my favorite color too!!")
 
 ###Bonus Questions
 
-###Create variable x=5 and y='5' and add them together to get '5+5' and print to the console
+#1) Create variable x=5 and y='5' and add them together to get '5+5' and print to the console. HINT: You need a function to convert between data types
 
 
 
 
 
-###Create variable x=5 and y='5' and add them together to get 10 and print to the console
+#2) Create variable x=5 and y='5' and add them together to get 10 and print to the console HINT: You need a function to convert between data types
 
 
 
 
 
-###Print "Hello World" in one print function so that "Hello" and "World" are on different lines
+#3) Print "Hello World" in one print function so that "Hello" and "World" are on different lines
 
 
 
 
 
-###Print the string "Congrats you did it!!!" 1,000 times
+#5) Print the string "Congrats you did it!!!" 1,000 times
 
 
 
